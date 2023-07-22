8a3ab0127dcb483d9b33ff6f320a2655462836c3
Galitsky Dmytro <melles.android@gmail.com>
2023-07-22 12:45:11 +0300

    Optimize code for DBMCPUkrainianChild function
    In this commit, I optimized the code for the DBMCPUkrainianChild function in the DBM-CPUkrainian addon. The function was responsible for inserting sound data into the DBM.Counts table. The original code had repetitive inserts for each sound entry, which made it less maintainable.
    
    To improve the code, I created a table called soundData that holds the common data for each sound entry. Then, I used a loop to iterate over soundData and insert each element into the DBM.Counts table using the tinsert function. This refactoring reduced duplication and improved readability, making it easier to manage sound entries in the future.
    
    By optimizing the code in this way, we ensure that the function remains efficient and scalable, and it also follows best practices for code organization.
    
    Changes made:
    - Created a table soundData to store sound entry data.
    - Used a loop to insert soundData elements into DBM.Counts.
    - Improved code readability and maintainability.
    
    This commit should help make the DBM-CPUkrainian addon more maintainable and easier to contribute to in the future.

--- CountLoadList.lua
+++ CountLoadList.lua
@@ -3,9 +3,48 @@ local inserted = false
 
 function DBMCPUkrainianChild()
 	if inserted then return end
-	tinsert(DBM.Counts, {	text	= "Ukrainian",			value 	= "ua_default",		path = "Interface\\AddOns\\DBM-CPUkrainian\\Default\\",		max = 5})
-	tinsert(DBM.Counts, {	text	= "Ukrainian Child",			value 	= "ua_child",		path = "Interface\\AddOns\\DBM-CPUkrainian\\Child\\",		max = 5})
-	tinsert(DBM.Counts, {	text	= "Ukrainian Strikes",			value 	= "ua_strikes",		path = "Interface\\AddOns\\DBM-CPUkrainian\\Strikes\\",		max = 5})
-	tinsert(DBM.Counts, {	text	= "Ukrainian Tik Tak",			value 	= "ua_tiktak",		path = "Interface\\AddOns\\DBM-CPUkrainian\\TikTak\\",		max = 5})
+
+	local soundData = {
+		{
+			text = "Ukrainian",
+			value = "ua_default",
+			path =
+			"Interface\\AddOns\\DBM-CPUkrainian\\Sound\\Default\\",
+			max = 5
+		},
+		{
+			text = "Ukrainian Child",
+			value = "ua_child",
+			path =
+			"Interface\\AddOns\\DBM-CPUkrainian\\Sound\\Child\\",
+			max = 5
+		},
+		{
+			text = "Ukrainian Strikes",
+			value = "ua_strikes",
+			path =
+			"Interface\\AddOns\\DBM-CPUkrainian\\Sound\\Strikes\\",
+			max = 5
+		},
+		{
+			text = "Ukrainian Tik Tak",
+			value = "ua_tiktak",
+			path =
+			"Interface\\AddOns\\DBM-CPUkrainian\\Sound\\TikTak\\",
+			max = 5
+		},
+		{
+			text = "Ukrainian Verhovha Rada",
+			value = "ua_vr",
+			path =
+			"Interface\\AddOns\\DBM-CPUkrainian\\Sound\\VerhovhaRada\\",
+			max = 5
+		}
+	}
+
+	for _, data in ipairs(soundData) do
+		tinsert(DBM.Counts, data)
+	end
+
 	inserted = true
 end
